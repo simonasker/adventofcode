ac7603e9cd3042a5c31f38efd48398e9185fff4a
Simon Andersson <simon.asker@gmail.com>
2015-12-09 21:45:28 +0100

    Calculate number of houses visited

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -9,13 +10,20 @@ fn main() {
     let mut input_string = String::new();
     input_file.read_to_string(&mut input_string).unwrap();
 
+    let mut coords = HashSet::new();
+    let mut current_pos = (0, 0);
+    coords.insert(current_pos);
     for c in input_string.chars() {
         match c {
-            '^' => println!("up"),
-            '>' => println!("right"),
-            'v' => println!("down"),
-            '<' => println!("left"),
+            '^' => current_pos.1 += 1,
+            '>' => current_pos.0 += 1,
+            'v' => current_pos.1 -= 1,
+            '<' => current_pos.0 -= 1,
             _   => continue,
         }
+        coords.insert(current_pos);
     }
+
+    println!("Number of houses visited: {}", coords.len());
+
 }
