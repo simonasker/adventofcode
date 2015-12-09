19e00bc6a10897555b4589d814b770cb83d1da48
Simon Andersson <simon.asker@gmail.com>
2015-12-09 21:23:47 +0100

    Read and print input file

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+
+    let mut input_file = File::open(&args[1]).unwrap();
+    let mut input_string = String::new();
+    input_file.read_to_string(&mut input_string).unwrap();
+
+    println!("{}", input_string);
 }
