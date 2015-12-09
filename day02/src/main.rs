b95695d3ce460026f80e88a0a8918b4e9f186d0d
Simon Andersson <simon.asker@gmail.com>
2015-12-09 20:38:07 +0100

    Calculate ribbon length

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     let mut input_string = String::new();
     input_file.read_to_string(&mut input_string).unwrap();
 
-    let mut total_area = 0;
+    let mut total_paper = 0;
+    let mut total_ribbon = 0;
     for line in input_string.lines() {
         let split = line.split('x');
         let mut v: Vec<i32> = split.map(|x| x.parse::<i32>().unwrap()).collect();
@@ -20,9 +21,16 @@ fn main() {
         let w = v[1];
         let h = v[2];
 
+        // Wrapping paper
         let surface_area = 2 * l * w + 2 * w * h + 2 * h * l;
         let slack = l * w;
-        total_area = total_area + surface_area + slack;
+        total_paper = total_paper + surface_area + slack;
+
+        // Ribbon
+        let ribbon = 2 * l + 2 * w;
+        let bow = l * w * h;
+        total_ribbon = total_ribbon + ribbon + bow;
     }
-    println!("Total area: {}", total_area);
+    println!("Total paper area: {}", total_paper);
+    println!("Total ribbon length: {}", total_ribbon);
 }
