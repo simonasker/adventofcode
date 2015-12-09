753026c3334125a31b727dd32c962fc999b6bf19
Simon Andersson <simon.asker@gmail.com>
2015-12-09 17:09:02 +0100

    Get input string from command line

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,13 +1,15 @@
+use std::env;
+
 fn main() {
-    let input = "(((";
+    let args: Vec<_> = env::args().collect();
 
     let mut floor = 0;
-    for c in input.chars() {
+    for c in args[1].chars() {
         match c {
             '(' => floor += 1,
             ')' => floor -= 1,
             _   => continue,
         }
     }
-    println!("{}", floor);
+    println!("Floor: {}", floor);
 }
