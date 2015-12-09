a085367d9f86da7290fe4b935152e8141f41f70c
Simon Andersson <simon.asker@gmail.com>
2015-12-09 17:20:42 +0100

    Check when first entering basement

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,11 +4,16 @@ fn main() {
     let args: Vec<_> = env::args().collect();
 
     let mut floor = 0;
+    let mut enter_basement = 0;
     for c in args[1].chars() {
         match c {
             '(' => floor += 1,
             ')' => floor -= 1,
             _   => continue,
+        }
+        if floor < 0 && enter_basement == 0 {
+            enter_basement = 1;
+            println!("ENTERED BASEMENT");
         }
     }
     println!("Floor: {}", floor);
