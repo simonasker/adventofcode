c920d7b40f3f1847397864d357992c4e074fbbc5
Simon Andersson <simon.asker@gmail.com>
2015-12-09 19:24:26 +0100

    Get input from file

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,20 +1,27 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
 
+    let mut input_file = File::open(&args[1]).unwrap();
+    let mut input_string = String::new();
+    input_file.read_to_string(&mut input_string).unwrap();
+
     let mut floor = 0;
     let mut enter_basement = 0;
-    for c in args[1].chars() {
+
+    for (pos, c) in input_string.chars().enumerate() {
         match c {
             '(' => floor += 1,
             ')' => floor -= 1,
             _   => continue,
         }
         if floor < 0 && enter_basement == 0 {
-            enter_basement = 1;
-            println!("ENTERED BASEMENT");
+            enter_basement = pos + 1;
         }
     }
     println!("Floor: {}", floor);
+    println!("Entered basement at pos: {}", enter_basement);
 }
