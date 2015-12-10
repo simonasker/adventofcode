8586b5c0ca4f815b39b8f4d63dcb83721e55f5c3
Simon Andersson <simon.asker@gmail.com>
2015-12-10 19:39:44 +0100

    Loop and concatenate input with incrementing int

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -3,4 +3,17 @@ use std::env;
 fn main() {
     let args: Vec<_> = env::args().collect();
     println!("You entered: {}", args[1]);
+    let input = &args[1];
+
+    let mut n = 1;
+    loop {
+        let s = format!("{}{}", input, n);
+        println!("{}", s);
+        if n == 10 {
+            break;
+        }
+        n += 1;
+    }
+
+    println!("Anwer: {}", n);
 }
