9d277cc6a1a353525b657784550b506effa620e3
Simon Andersson <simon.asker@gmail.com>
2015-12-11 21:03:43 +0100

    Get coords from instruction string

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -4,38 +4,43 @@ fn index(x: usize, y: usize) -> usize {
     y * SIZE + x
 }
 
+fn to_coord(s: &str) -> Vec<usize> {
+    let r: Vec<usize> = s
+        .split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
+    return r;
+}
+
 fn main() {
     let mut grid: Vec<u8> = vec![0; SIZE * SIZE];
 
-    let instruction = "toggle 0,0 through 2,2";
+    let instruction = "turn on 0,0 through 2,2";
     let v: Vec<&str> = instruction.split_whitespace().collect();
 
     let mut op: &str = "";
-    let mut a: &str = "";
-    let mut b: &str = "";
+    let mut a: Vec<usize> = vec![];
+    let mut b: Vec<usize> = vec![];
 
     match v[0] {
         "toggle" => {
             op = "toggle";
-            a = v[1];
-            b = v[3];
+            a = to_coord(v[1]);
+            b = to_coord(v[3]);
         },
         "turn" => {
             op = v[1];
-            a = v[2];
-            b = v[4];
+            a = to_coord(v[2]);
+            b = to_coord(v[4]);
         },
         _ => println!("Invalid instruction"),
     }
 
-    println!("{}, {}, {}", op, a, b);
+    println!("{:?}, {:?}, {:?}", op, a, b);
 
 
-    let a = (0, 0);
-    let b = (2, 2);
-
-    for x in a.0..b.0+1 {
-        for y in a.1..b.1+1 {
+    for x in a[0]..b[0] + 1 {
+        for y in a[1]..b[1] + 1 {
             grid[index(x, y)] = 1;
         }
     }
