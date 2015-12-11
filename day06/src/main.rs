c523f1e82916de7234dfccfcfa4608d6b4bb7afb
Simon Andersson <simon.asker@gmail.com>
2015-12-11 20:24:08 +0100

    Represent grid as byte array

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,3 +1,13 @@
+const SIZE: usize = 3;
+
+fn index(x: usize, y: usize) -> usize {
+    y * SIZE + x
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut grid: Vec<u8> = vec![0; SIZE * SIZE];
+
+    grid[index(1, 1)] = 1;
+
+    println!("{:?}", grid);
 }
