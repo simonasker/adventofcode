65a2e9be6961bbf852402a4740052658b6757849
Simon Andersson <simon.asker@gmail.com>
2015-12-11 20:30:41 +0100

    Fill subgrid given two coordinates

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -7,7 +7,15 @@ fn index(x: usize, y: usize) -> usize {
 fn main() {
     let mut grid: Vec<u8> = vec![0; SIZE * SIZE];
 
-    grid[index(1, 1)] = 1;
+    println!("{:?}", grid);
+    let a = (0, 0);
+    let b = (2, 2);
+
+    for x in a.0..b.0+1 {
+        for y in a.1..b.1+1 {
+            grid[index(x, y)] = 1;
+        }
+    }
 
     println!("{:?}", grid);
 }
