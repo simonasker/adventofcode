a835b0990fd381d6757c76f41f0d7612ed63b3de
Simon Andersson <simon.asker@gmail.com>
2015-12-11 21:15:51 +0100

    Do as the instruction says

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -15,7 +15,7 @@ fn to_coord(s: &str) -> Vec<usize> {
 fn main() {
     let mut grid: Vec<u8> = vec![0; SIZE * SIZE];
 
-    let instruction = "turn on 0,0 through 2,2";
+    let instruction = "toggle 0,0 through 2,2";
     let v: Vec<&str> = instruction.split_whitespace().collect();
 
     let mut op: &str = "";
@@ -38,10 +38,18 @@ fn main() {
 
     println!("{:?}, {:?}, {:?}", op, a, b);
 
-
     for x in a[0]..b[0] + 1 {
         for y in a[1]..b[1] + 1 {
-            grid[index(x, y)] = 1;
+            match op {
+                "on"     => grid[index(x, y)] = 1,
+                "off"    => grid[index(x, y)] = 0,
+                "toggle" => if grid[index(x, y)] == 0 {
+                    grid[index(x, y)] = 1;
+                } else {
+                    grid[index(x, y)] = 0;
+                },
+                _ => println!("Invalid instruction"),
+            }
         }
     }
 
