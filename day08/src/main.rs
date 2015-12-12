0d11807621213298357009101ea466c1caf08d3a
Simon Andersson <simon.asker@gmail.com>
2015-12-12 17:35:03 +0100

    Encode the strings to solve part 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -12,24 +12,51 @@ fn main() {
     let mut input_file = File::open(&args[1]).unwrap();
     let mut input_string = String::new();
     input_file.read_to_string(&mut input_string).unwrap();
+    let part = args[2].parse::<i32>().unwrap();
 
-    let mut total_code_chars = 0;
-    let mut total_string_chars = 0;
 
-    for line in input_string.lines() {
-        total_code_chars += line.len();
+    if part == 1 {
+        let mut total_code_chars = 0;
+        let mut total_string_chars = 0;
 
-        // Remove the surrounding quotes
-        let mut new_line = line[1 .. line.len() - 1].to_owned();
+        for line in input_string.lines() {
+            total_code_chars += line.len();
 
-        // Remove all escaped characters
-        let re = Regex::new(r"\\(\x22|\\|x[:xdigit:]{2})").unwrap();
-        new_line = re.replace_all(&new_line, "#");
+            // Remove the surrounding quotes
+            let mut new_line = line[1 .. line.len() - 1].to_owned();
 
-        total_string_chars += new_line.len();
+            // Remove all escaped characters
+            let re = Regex::new(r"\\(\x22|\\|x[:xdigit:]{2})").unwrap();
+            new_line = re.replace_all(&new_line, "#");
+
+            total_string_chars += new_line.len();
+        }
+
+        println!("Total number of code characters: {}", total_code_chars);
+        println!("Total number of string characters: {}", total_string_chars);
+        println!("Answer: {}", total_code_chars - total_string_chars);
+
+    } else if part == 2 {
+        let mut total_orig_chars = 0;
+        let mut total_encoded_chars = 0;
+
+        for line in input_string.lines() {
+            total_orig_chars += line.len();
+
+            let mut new_line = line.to_owned();
+
+            // Replace backslashes and quotes with two #s
+            let re = Regex::new(r"\\|\x22").unwrap();
+            new_line = re.replace_all(&new_line, "##");
+
+            // Add surrounding quotes
+            new_line.push('"');
+            new_line.insert(0, '"');
+
+            total_encoded_chars += new_line.len();
+        }
+        println!("Total number of string characters: {}", total_orig_chars);
+        println!("Total number of encoded characters: {}", total_encoded_chars);
+        println!("Answer: {}", total_encoded_chars - total_orig_chars);
     }
-
-    println!("Total number of code characters: {}", total_code_chars);
-    println!("Total number of string characters: {}", total_string_chars);
-    println!("Answer: {}", total_code_chars - total_string_chars);
 }
