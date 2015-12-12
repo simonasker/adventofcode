c0b99cbab5ba93c74124fc95d4f2e6ac0781d3c6
Simon Andersson <simon.asker@gmail.com>
2015-12-12 17:52:54 +0100

    Store and count all unique locations

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -9,9 +10,17 @@ fn main() {
     let mut input_string = String::new();
     input_file.read_to_string(&mut input_string).unwrap();
 
+    let mut locations = HashSet::new();
+
     for line in input_string.lines() {
         let v: Vec<&str> = line.split_whitespace().collect();
 
+        locations.insert(v[0]);
+        locations.insert(v[2]);
+
         println!("{:?}", v);
     }
+
+    println!("{:?}", locations);
+    println!("{:?}", locations.len());
 }
