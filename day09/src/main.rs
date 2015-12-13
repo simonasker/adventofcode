5d64f2d4b89c1995311206829f56400c67b558c5
Simon Andersson <simon.asker@gmail.com>
2015-12-13 09:59:29 +0100

    Store all distances in a HashMap

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -11,6 +11,7 @@ fn main() {
     input_file.read_to_string(&mut input_string).unwrap();
 
     let mut locations = HashSet::new();
+    let mut distances = HashMap::new();
 
     for line in input_string.lines() {
         let v: Vec<&str> = line.split_whitespace().collect();
@@ -18,9 +19,16 @@ fn main() {
         locations.insert(v[0]);
         locations.insert(v[2]);
 
+        distances.insert((v[0], v[2]), v[4]);
+        distances.insert((v[2], v[0]), v[4]);
+
         println!("{:?}", v);
     }
 
     println!("{:?}", locations);
     println!("{:?}", locations.len());
+    println!("{:?}", distances);
+
+    println!("{:?}", distances.get(&("Tristram", "Norrath")));
+    println!("{:?}", distances.get(&("Norrath", "Tristram")));
 }
