aa85f10a92d29d6e7b713e09d02e5f8e9063d301
Simon Andersson <simon.asker@gmail.com>
2015-12-13 14:05:13 +0100

    Compute the same result 40 times

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,20 +5,23 @@ fn main() {
     let input_string = &args[1];
     println!("Input: {}", input_string);
 
-    let mut groups: Vec<Vec<char>> = Vec::new();
-    let mut prev: char = ' ';
-    for c in input_string.chars() {
-        if c != prev {
-            groups.insert(0, Vec::new());
+    let iterations = 40;
+    for i in 1 .. iterations + 1 {
+        let mut groups: Vec<Vec<char>> = Vec::new();
+        let mut prev: char = ' ';
+        for c in input_string.chars() {
+            if c != prev {
+                groups.insert(0, Vec::new());
+            }
+            groups[0].push(c);
+            prev = c;
         }
-        groups[0].push(c);
-        prev = c;
-    }
-    groups.reverse();
+        groups.reverse();
 
-    let mut new_input = String::new();
-    for group in groups {
-        new_input.push_str(&format!("{}{}", group.len(), group[0]));
+        let mut new_input = String::new();
+        for group in groups {
+            new_input.push_str(&format!("{}{}", group.len(), group[0]));
+        }
+        println!("{:02}: {}", i, new_input);
     }
-    println!("Result: {}", new_input);
 }
