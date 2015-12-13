e8ffc46d9932652a51b868b9f769b00d7347c842
Simon Andersson <simon.asker@gmail.com>
2015-12-13 11:30:58 +0100

    Implement prototype of Heap's algorithm

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -3,6 +3,23 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::collections::{HashSet, HashMap};
 
+
+fn permutations(n: usize, a: &mut Vec<i32>) {
+    if n == 1 {
+        println!("Output: {:?}", a);
+    } else {
+        for i in 0 .. n-1 {
+            permutations(n-1, a);
+            if n % 2 == 0 {
+                a.swap(i, n-1);
+            } else {
+                a.swap(0, n-1);
+            }
+        }
+        permutations(n-1, a);
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
 
@@ -21,14 +38,14 @@ fn main() {
 
         distances.insert((v[0], v[2]), v[4]);
         distances.insert((v[2], v[0]), v[4]);
-
-        println!("{:?}", v);
     }
 
-    println!("{:?}", locations);
-    println!("{:?}", locations.len());
-    println!("{:?}", distances);
+    let mut a = vec![0, 1, 2];
+    // println!("Before: {:?}", a);
 
-    println!("{:?}", distances.get(&("Tristram", "Norrath")));
-    println!("{:?}", distances.get(&("Norrath", "Tristram")));
+    permutations(3, &mut a);
+
+    /* let perms = permutations(&mut a); */
+    /* println!("After: {:?}", perms); */
+
 }
