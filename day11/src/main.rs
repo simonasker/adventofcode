9c0d3dc38f6558ec9569951c6b5916d17be8cd70
Simon Andersson <simon.asker@gmail.com>
2015-12-13 22:02:00 +0100

    Read and print out input string

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let input_string = args[1].clone();
+    println!("Input: {}", input_string);
 }
