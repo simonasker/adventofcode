1623c1a8c0bdc3f6b71c5721ade94c8f6081cd61
Simon Andersson <simon.asker@gmail.com>
2015-12-13 13:47:10 +0100

    Place each character in its own group

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let input_string = &args[1];
+    println!("{}", input_string);
+
+    let mut groups: Vec<Vec<char>> = Vec::new();
+    for c in input_string.chars() {
+        groups.insert(0, Vec::new());
+        groups[0].push(c);
+    }
+
+    println!("{:?}", groups);
 }
