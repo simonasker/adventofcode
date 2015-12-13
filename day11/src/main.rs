fbdc88972cd014dd2cc148781aa7b5ef40169ae9
Simon Andersson <simon.asker@gmail.com>
2015-12-13 22:24:52 +0100

    Increment the last byte in a string

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,7 +1,22 @@
 use std::env;
 
+
+/* fn increment_string(s: &str) -> &str { */
+
+/* } */
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     let input_string = args[1].clone();
     println!("Input: {}", input_string);
+    println!("Bytes: {:?}", input_string.as_bytes());
+
+    println!("Incrementing...");
+    let mut bytes = input_string.into_bytes();
+    let len = bytes.len();
+    bytes[len - 1] += 1;
+    println!("Bytes: {:?}", bytes);
+
+    let s = String::from_utf8(bytes).unwrap();
+    println!("Output: {}", s);
 }
