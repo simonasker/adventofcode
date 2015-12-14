9ac2613a1a1eb9380a66e9a8b6af3f624dcae735
Simon Andersson <simon.asker@gmail.com>
2015-12-14 22:21:51 +0100

    Check for all conditions from part 1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -15,23 +15,28 @@ fn increment_string(s: String) -> String {
 }
 
 fn is_valid(s: &String) -> bool {
-    let mut increasing_straight = false;
-
-    let bytes = s.clone().into_bytes();
-    for i in 2..bytes.len() {
-        if bytes[i] == bytes[i-1] + 1 && bytes[i] == bytes[i-2] + 2 {
-            increasing_straight = true;
-        }
-    }
-
     for c in s.chars() {
         match c {
             'i' | 'o' | 'l' => return false,
             _ => continue,
         };
     }
-
-    increasing_straight
+    let bytes = s.clone().into_bytes();
+    let mut increasing_straight = false;
+    for i in 2..bytes.len() {
+        if bytes[i] == bytes[i-1] + 1 && bytes[i] == bytes[i-2] + 2 {
+            increasing_straight = true;
+        }
+    }
+    let mut pairs = 0;
+    let mut prev_pair = 0;
+    for i in 1..bytes.len() {
+        if bytes[i] == bytes[i-1] && bytes[i] != prev_pair {
+            pairs += 1;
+            prev_pair = bytes[i];
+        }
+    }
+    increasing_straight && pairs >= 2
 }
 
 fn main() {
@@ -41,9 +46,9 @@ fn main() {
     println!("Input: {}", input_string);
     loop {
         input_string = increment_string(input_string);
-        println!("Output: {}", input_string);
         if is_valid(&input_string) {
             break;
         }
     }
+    println!("Result: {}", input_string);
 }
