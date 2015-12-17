46476b4482280938bedb19dec7f3321ad0ecab1b
Simon Andersson <simon.asker@gmail.com>
2015-12-17 19:06:05 +0100

    Read and print each line in the input

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    let mut input_file = File::open(&args[1]).unwrap();
+    let mut input_string = String::new();
+    input_file.read_to_string(&mut input_string).unwrap();
+
+    for line in input_string.lines() {
+        println!("{}", line);
+        let v: Vec<&str> = line.split_whitespace().collect();
+    }
 }
