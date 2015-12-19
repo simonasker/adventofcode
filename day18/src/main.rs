47f1104db463906a1e11bcf9572632bbe60018ac
Simon Andersson <simon.asker@gmail.com>
2015-12-19 20:04:57 +0100

    Create a grid based on input file

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -2,6 +2,14 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
+const SIZE: usize = 10;
+
+fn print_grid(grid: [[i32; SIZE]; SIZE]) {
+    for row in grid.iter() {
+        println!("{:?}", row);
+    }
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
 
@@ -9,7 +17,15 @@ fn main() {
     let mut input_string = String::new();
     input_file.read_to_string(&mut input_string).unwrap();
 
-    for line in input_string.lines() {
-        println!("{}", line);
+    let mut grid = [[0; SIZE]; SIZE];
+
+    for (i, l) in input_string.lines().enumerate() {
+        for (j, c) in l.chars().enumerate() {
+            if c == '#' {
+                grid[i][j] = 1;
+            }
+        }
     }
+
+    print_grid(grid);
 }
