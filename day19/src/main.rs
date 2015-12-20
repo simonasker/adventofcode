c55d8b165d629de5ac6d262b698a67ed0933f2bb
Simon Andersson <simon.asker@gmail.com>
2015-12-20 10:11:51 +0100

    Find target in one step

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -12,11 +12,12 @@ fn main() {
 
     let mut replacements = HashMap::new();
 
-    let mut molecule = String::new();
+    let mut target = String::new();
+    let mut molecule = "e".to_string();
     let mut replacements_done = false;
     for line in input_string.lines() {
         if replacements_done {
-            molecule = line.to_string();
+            target = line.to_string();
         } else if line.len() == 0 {
             replacements_done = true;
         } else {
@@ -29,30 +30,43 @@ fn main() {
         }
     }
 
+    println!("Target: {}", target);
+
     let mut distinct_molecules = HashSet::new();
     let mol_len = molecule.len();
+    let mut iterations = 0;
 
-    for i in 0..mol_len {
-        let s = &molecule[i..i+1];
-        if let Some(reps) = replacements.get(s) {
-            for r in reps {
-                let new_mol = format!("{}{}{}",
-                    &molecule[0..i], r, &molecule[i+1..mol_len]);
-                distinct_molecules.insert(new_mol);
+    'main: loop {
+        iterations += 1;
+
+        for i in 0..mol_len {
+            let s = &molecule[i..i+1];
+            if let Some(reps) = replacements.get(s) {
+                for r in reps {
+                    let new_mol = format!("{}{}{}",
+                        &molecule[0..i], r, &molecule[i+1..mol_len]);
+                    if new_mol == target {
+                        println!("Found target: {}", new_mol);
+                        break 'main;
+                    }
+                    distinct_molecules.insert(new_mol);
+                }
+            }
+        }
+
+        for i in 1..mol_len {
+            let s = &molecule[i-1..i+1];
+            if let Some(reps) = replacements.get(s) {
+                for r in reps {
+                    let new_mol = format!("{}{}{}",
+                        &molecule[0..i-1], r, &molecule[i+1..mol_len]);
+                    distinct_molecules.insert(new_mol);
+                }
             }
         }
     }
 
-    for i in 1..mol_len {
-        let s = &molecule[i-1..i+1];
-        if let Some(reps) = replacements.get(s) {
-            for r in reps {
-                let new_mol = format!("{}{}{}",
-                    &molecule[0..i-1], r, &molecule[i+1..mol_len]);
-                distinct_molecules.insert(new_mol);
-            }
-        }
-    }
-
+    println!("New molecules: {:?}", distinct_molecules);
     println!("Number of new molecules: {:?}", distinct_molecules.len());
+    println!("Iterations: {}", iterations);
 }
