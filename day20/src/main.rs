b34825682c666d7b5033bcfe4e4708bc1a553844
Simon Andersson <simon.asker@gmail.com>
2015-12-23 20:42:30 +0100

    Faster solution

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,14 +1,24 @@
+use std::collections::HashSet;
+
 fn main() {
     for house in 1.. {
         let mut presents = 0;
-        let elf_cap = house / 2 + 1;
-        for elf in 1..elf_cap {
-            if house % elf == 0 {
-                presents += elf * 10;
+
+        let sq = (house as f32).sqrt() as i32;
+
+        let mut elves = HashSet::new();
+        for i in 1 .. sq + 1 {
+            if house % i == 0 {
+                elves.insert(i);
+                elves.insert(house / i);
             }
         }
-        presents += house * 10;
-        println!("House {} got {} presents. ({})", house, presents, elf_cap);
+
+        for e in elves {
+            presents += e * 10;
+        }
+
+        println!("House {} got {} presents.", house, presents);
         if presents >= 36000000 {
             break;
         }
