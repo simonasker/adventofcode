1934bfd77d7aef4be4d5cf468fb8ccf0e9c65633
Simon Andersson <simon.asker@gmail.com>
2015-12-23 19:16:52 +0100

    Replicate the example

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     for house in 1..10 {
-        let presents = 0;
+        let mut presents = 0;
+        for elf in 1..house+1 {
+            if house % elf == 0 {
+                presents += elf * 10;
+            }
+        }
         println!("House {} got {} presents.", house, presents);
     }
 }
