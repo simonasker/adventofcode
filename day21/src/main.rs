b1357ece14adb46e58a5c5810633ab314567b2ed
Simon Andersson <simon.asker@gmail.com>
2015-12-23 21:26:18 +0100

    Create list of items

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,6 +1,18 @@
+extern crate regex;
+
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+
+use regex::Regex;
+
+#[derive(Debug)]
+struct Item {
+    name: String,
+    cost: i32,
+    damage: i32,
+    armor: i32,
+}
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -13,8 +25,22 @@ fn main() {
     boss_file.read_to_string(&mut boss_string).unwrap();
 
 
+    let mut items: Vec<Item> = Vec::new();
+    let re = Regex::new(r"(\w+(?: \+\d)?)\s*(\d+)\s*(\d+)\s*(\d+)").unwrap();
     for line in shop_string.lines() {
-        println!("{}", line);
+        if re.is_match(line) {
+            let caps = re.captures(line).unwrap();
+            items.push(Item {
+                name: caps.at(1).unwrap().to_string(),
+                cost: caps.at(2).unwrap().parse::<i32>().unwrap(),
+                damage: caps.at(3).unwrap().parse::<i32>().unwrap(),
+                armor: caps.at(4).unwrap().parse::<i32>().unwrap(),
+            });
+        }
+    }
+
+    for item in items {
+        println!("{:?}", item);
     }
 
     for line in boss_string.lines() {
