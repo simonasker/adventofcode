f189298d7316201c1f00a275acf8a294a55524d9
Simon Andersson <simon.asker@gmail.com>
2015-12-23 21:08:28 +0100

    Read and print the lines of the input files

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+
+    let mut shop_file = File::open(&args[1]).unwrap();
+    let mut boss_file = File::open(&args[2]).unwrap();
+    let mut shop_string = String::new();
+    let mut boss_string = String::new();
+    shop_file.read_to_string(&mut shop_string).unwrap();
+    boss_file.read_to_string(&mut boss_string).unwrap();
+
+
+    for line in shop_string.lines() {
+        println!("{}", line);
+    }
+
+    for line in boss_string.lines() {
+        println!("{}", line);
+    }
 }
