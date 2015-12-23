332182b2d902ae80ab3add7657cf656a128c2d97
Simon Andersson <simon.asker@gmail.com>
2015-12-23 19:52:44 +0100

    Probably working but very slow

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,11 +1,16 @@
 fn main() {
-    for house in 1..10 {
+    for house in 1.. {
         let mut presents = 0;
-        for elf in 1..house+1 {
+        let elf_cap = house / 2 + 1;
+        for elf in 1..elf_cap {
             if house % elf == 0 {
                 presents += elf * 10;
             }
         }
-        println!("House {} got {} presents.", house, presents);
+        presents += house * 10;
+        println!("House {} got {} presents. ({})", house, presents, elf_cap);
+        if presents >= 36000000 {
+            break;
+        }
     }
 }
