5cd9a936c2d0ec535b224a869aa2ae8d5831eea9
Simon Andersson <simon.asker@gmail.com>
2015-12-24 09:07:32 +0100

    Load the boss

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Debug)]
+struct Boss {
+    hp: i32,
+    damage: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+
+    let mut boss_file = File::open(&args[1]).unwrap();
+    let mut boss_string = String::new();
+    boss_file.read_to_string(&mut boss_string).unwrap();
+
+    let boss_v: Vec<&str> = boss_string.split_whitespace().collect();
+    let boss = Boss {
+        hp: boss_v[2].parse::<i32>().unwrap(),
+        damage: boss_v[4].parse::<i32>().unwrap(),
+    };
+
+    println!("{:?}", boss);
 }
